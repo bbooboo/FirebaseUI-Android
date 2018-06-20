8fd485e3a131c24ae641172c066f55968fb940e8
bbooboo <40388774+bbooboo@users.noreply.github.com>
2018-06-20 14:38:44 +1000

    Delete settings.gradle.kts

--- settings.gradle.kts
+++
@@ -1,9 +0,0 @@
-include(
-        ":app", ":library",
-
-        ":auth",
-        ":common", ":firestore", ":database",
-        ":storage",
-
-        ":proguard-tests", ":internal:lint", ":internal:lintchecks"
-)
